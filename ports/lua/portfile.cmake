83f62c4a259de77798611f52c66b4caa211efc69
Levin Li <lilinfeng303@outlook.com>
2021-03-25 18:17:31 +0800

    lua 5.1 fix

--- ports/lua/portfile.cmake
+++ ports/lua/portfile.cmake
@@ -1,12 +1,11 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.lua.org/ftp/lua-5.4.2.tar.gz"
-    FILENAME "lua-5.4.2.tar.gz"
-    SHA512 9454a6ffd973598f2f4a2399834c31c4d5090bd12e716776e3189aa57760319d114ee64a8338bbc2ef5e08150bf0adc2ad94a1b2677f38538a43359969d4d920
+    URLS "https://www.lua.org/ftp/lua-5.1.5.tar.gz"
+    FILENAME "lua-5.1.5.tar.gz"
+    SHA512 0142fefcbd13afcd9b201403592aa60620011cc8e8559d4d2db2f92739d18186860989f48caa45830ff4f99bfc7483287fd3ff3a16d4dec928e2767ce4d542a9
 )
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    PATCHES vs2015-impl-c99.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
