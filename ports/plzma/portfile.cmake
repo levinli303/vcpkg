8a25b82210d67e6df9b5b030638ed46a9488bb3e
Levin Li <lilinfeng303@outlook.com>
2022-10-21 20:07:14 +0800

    Update Plzma

--- ports/plzma/portfile.cmake
+++ ports/plzma/portfile.cmake
@@ -1,10 +1,8 @@
-vcpkg_fail_port_install(ON_TARGET "UWP")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO levinli303/PLzmaSDK
-    REF 91455a466c8c849483201a2f3de5fd96d6000c18
-    SHA512 3f18dc94104fc79d247d4106097cd581709c73fbeb1d8f5861cd1d12a9e4d6a557bec5a2228217455bc545ae201335943353d0ec6625639459fe98bbc3fbcd8f
+    REF 9a4e0e8f4a4c68da04b2146e4fe7dcb2662257e3
+    SHA512 319588e5f6f4ee32f3af133887e003ad3efc47e07913050f269a446110614054b91ae0966336637ddc4bf27cee2244f0cb77150c0b421f0a38f88d4967da894a
     HEAD_REF master
 )
 
@@ -16,8 +14,6 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_copy_pdbs()
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
@@ -26,5 +22,7 @@ file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin")
 
 file(RENAME "${CURRENT_PACKAGES_DIR}/lib/libplzma.dll" "${CURRENT_PACKAGES_DIR}/bin/libplzma.dll")
 file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/libplzma.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/libplzma.dll")
+
+vcpkg_copy_pdbs()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
